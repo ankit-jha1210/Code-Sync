77d9031a475dae493e500d121c483a23018f6e68
Ankit Jha <jhaa11701@gmail.com>
2022-08-05 11:41:17 +0530

    added options to change theme

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,21 +1,29 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import React from "react";
 import CodeMirror from "codemirror";
 import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
 import 'codemirror/theme/dracula.css';
-import 'codemirror/mode/clike/clike';
+import 'codemirror/theme/tomorrow-night-bright.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/theme/material-darker.css';
+import 'codemirror/mode/clike/clike'
 import 'codemirror/addon/edit/closetag';
 import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/keymap/sublime';
 import ACTIONS from "../Actions";
 const Editor = ({ socketRef, roomId, onCodeChange }) => {
+    const themes = ['dracula', 'monokai', 'material', 'eclipse', 'tomorrow-night-bright', 'material-darker'];
     const editorRef = useRef();
     useEffect(() => {
         async function init() {
             editorRef.current = CodeMirror.fromTextArea(document.getElementById('codeEditor'), {
                 mode: { name: 'text/x-java' },
+                autoCloseTags: 'true',
+                keyMap: 'sublime',
                 theme: 'dracula',
-                autoCloseTags: true,
                 autoCloseBrackets: true,
+                placeholder: 'Please enter the JavaScript code.',
                 lineNumbers: true,
             })
             editorRef.current.on('change', (instance, changes) => {
@@ -45,6 +53,18 @@ const Editor = ({ socketRef, roomId, onCodeChange }) => {
             socketRef.current.off(ACTIONS.CODE_CHANGE);
         }
     }, [socketRef.current])
-    return <textarea id="codeEditor"></textarea>
+
+    const selectThemes = (e) => {
+        editorRef.current.setOption('theme', e.target.value);
+    }
+    return (<><textarea id="codeEditor">
+    </textarea>
+        <section id="header-container">
+            <select id="themeOptions" onChange={selectThemes}>
+                <option disabled selected>Theme</option>
+                {themes.map(theme => <option key={theme} value={theme}>{theme}</option>)}
+            </select>
+        </section>
+    </>)
 }
 export default Editor;
